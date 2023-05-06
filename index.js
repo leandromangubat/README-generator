9062e28928e7638f882c70760931d4edd1884a48
Leandro Mangubat <leandromangubat@hotmail.com>
2023-05-06 17:06:35 -0400

    added functionality to generate README

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
+
+
 const questions = [
   {
     type: "input",
@@ -13,6 +18,41 @@ const questions = [
     message: "Please describe the purpose of your project",
   },
   {
+    type: "input",
+    name: "screenshot",
+    message: "Please provide a relative path for the image you would like to use",
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Please provide a URL for your users to access the application",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "List your application's features here"
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Type your Github username"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributers to the application",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide a demonstration of your application"
+  },
+  {
     type: "checkbox",
     name: "license",
     message:"Please select a license for your project",
@@ -23,13 +63,24 @@ const questions = [
     name: "require",
     message: "Please list any project dependencies",
   },
+  {
+    type: "input",
+    name: "filename",
+    message: "Please enter the name of the README file",
+    
+  }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data); 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init(){
+  inquirer.prompt(questions).then((answers) => {
+writeToFile("./dist/README.md", generateMarkdown({ ...answers }));  
+})
+}
 // Function call to initialize app
 init();
